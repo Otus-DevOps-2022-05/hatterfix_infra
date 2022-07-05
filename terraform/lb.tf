3e226b5baac0f0b0e6d62ba820850eb939501b4e
Kirill Krivospitskiy <hatter@complat.ru>
2022-07-05 03:17:08 +0300

    added lb.tf & one net balancer:

---
+++ terraform/lb.tf
@@ -0,0 +1,31 @@
+resource "yandex_lb_target_group" "reddit_group" {
+  name      = "app-lb"
+  folder_id = var.folder_id
+
+  target {
+    subnet_id = var.subnet_id
+    address   = yandex_compute_instance.app.network_interface.0.ip_address
+  }
+}
+resource "yandex_lb_network_load_balancer" "lbalance" {
+  name = "test-balancer"
+  type = "external"
+
+  listener {
+    name = "checker"
+    port = 80
+    target_port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.reddit_group.id
+    healthcheck {
+      name = "healthcheck"
+        http_options {
+          port = 9292
+        }
+    }
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,7 @@ output "inst_app_external_IP" {
   description = "Instance app external IP:"
   value       = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
+output "balancer_external_IP" {
+  description = "Balancer app external IP:"
+  value       =  yandex_lb_network_load_balancer.lbalance.listener.*.external_address_spec[0].*.address
+}
