4c8aa0f9011d00c04c5e398e272df4ac1b3b07d2
Kirill Krivospitskiy <hatter@complat.ru>
2022-08-01 00:43:09 +0300

    add check port 27017 test:

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -16,3 +16,8 @@ def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+# check mongod port 27017 listening
+def check_mongod_port(host):
+    mongo_def = host.socket('tcp://0.0.0.0:27017')
+    assert mongo_def.is_listening
